3f0efc3786ac0eec219dc35501c871527cfdbd48
takuma02141978 <takuma02141978@yahoo.co.jp>
2016-06-24 10:08:34 +0900

    追加

---
+++ src/main/java/org/jenkinsci/plugins/stepcounter/util/Util.java
@@ -0,0 +1,16 @@
+package org.jenkinsci.plugins.stepcounter.util;
+
+import java.io.File;
+
+public class Util {
+	public static String getParentDirRelativePath(File file, String rootPath) {
+		String filePath = file.getParentFile().getAbsolutePath();
+		filePath = filePath.replaceAll("\\\\", "/");
+		rootPath = rootPath.replaceAll("\\\\", "/");
+		String result = filePath.replaceFirst(rootPath, "");
+		if (result.startsWith("/")) {
+			result = result.replaceFirst("/", "");
+		}
+		return result;
+	}
+}
